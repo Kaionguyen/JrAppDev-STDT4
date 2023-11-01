d0d9fd0ac0d90bf35df2b569ac068888314f615e
Kyle Nguyen <kylenguyencs@gmail.com>
2023-11-01 01:48:42 -0700

    Refactor: Cleaned up solution.

--- emasSuperComputer.js
+++ emasSuperComputer.js
@@ -1,5 +1,16 @@
+function plusInRange(cur, rows, cols, plusSize){
+    return cur[0] + plusSize < rows && cur[0] - plusSize >= 0 && cur[1] + plusSize < cols && cur[1] - plusSize >= 0;
+}
+
+function isChar(grid, cur, plusSize, char){
+    return grid[cur[0] + plusSize][cur[1]] === char && 
+           grid[cur[0] - plusSize][cur[1]] === char && 
+           grid[cur[0]][cur[1] + plusSize] === char && 
+           grid[cur[0]][cur[1] - plusSize] === char;
+}
+
 function twoPluses(grid) {
-    const newGrid = grid.map(row => row.split(''));
+    const gridArray = grid.map(row => row.split(''));
     const rows = grid.length;
     const cols = grid[0].length;
 
@@ -9,16 +20,14 @@ function twoPluses(grid) {
         for (let j = 0; j <= cols; j++) {
             let plusSize1 = 0;
 
-            while (i + plusSize1 < rows && i - plusSize1 >= 0 && j + plusSize1 < cols && j - plusSize1 >= 0 &&
-                newGrid[i + plusSize1][j] === "G" && newGrid[i - plusSize1][j] === "G" && newGrid[i][j + plusSize1] === "G" && newGrid[i][j - plusSize1] === "G") {
-                newGrid[i + plusSize1][j] = newGrid[i - plusSize1][j] = newGrid[i][j + plusSize1] = newGrid[i][j - plusSize1] = 'S';
+            while (plusInRange([i, j], rows, cols, plusSize1) && isChar(gridArray, [i, j], plusSize1, 'G')) {
+                gridArray[i + plusSize1][j] = gridArray[i - plusSize1][j] = gridArray[i][j + plusSize1] = gridArray[i][j - plusSize1] = 'S';
 
                 for (let k = 0; k <= rows; k++) {
                     for (let l = 0; l <= cols; l++) {
                         let plusSize2 = 0;
 
-                        while (k + plusSize2 < rows && k - plusSize2 >= 0 && l + plusSize2 < cols && l - plusSize2 >= 0 &&
-                            newGrid[k + plusSize2][l] === "G" && newGrid[k - plusSize2][l] === "G" && newGrid[k][l + plusSize2] === "G" && newGrid[k][l - plusSize2] === "G") {
+                        while (plusInRange([k, l], rows, cols, plusSize2) && isChar(gridArray, [k, l], plusSize2, 'G')) {
                             ans = Math.max(ans, (4 * plusSize1 + 1) * (4 * plusSize2 + 1));
                             plusSize2++;
                         }
@@ -29,9 +38,8 @@ function twoPluses(grid) {
             
             plusSize1 = 0;
             
-            while (i + plusSize1 < rows && i - plusSize1 >= 0 && j + plusSize1 < cols && j - plusSize1 >= 0 &&
-                newGrid[i + plusSize1][j] === "S" && newGrid[i - plusSize1][j] === "S" && newGrid[i][j + plusSize1] === "S" && newGrid[i][j - plusSize1] === "S") {
-                newGrid[i + plusSize1][j] = newGrid[i - plusSize1][j] = newGrid[i][j + plusSize1] = newGrid[i][j - plusSize1] = 'G';
+            while (plusInRange([i, j], rows, cols, plusSize1) && isChar(gridArray, [i, j], plusSize1, 'S')) {
+                gridArray[i + plusSize1][j] = gridArray[i - plusSize1][j] = gridArray[i][j + plusSize1] = gridArray[i][j - plusSize1] = 'G';
                 plusSize1++;
             }
         }
